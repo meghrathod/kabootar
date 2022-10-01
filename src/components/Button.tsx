f2792931da41b0b5994fd7882ec1f2e6317754cf
Akshit Garg <garg.akshit@gmail.com>
2022-10-01 19:43:33 +0530

    feat: add the home page
    Signed-off-by: Akshit Garg <garg.akshit@gmail.com>

---
+++ src/components/Button.tsx
@@ -0,0 +1,65 @@
+import { Component, JSX, createSignal } from "solid-js";
+
+const PrimaryButton: Component<{
+  children: JSX.Element;
+  class?: string;
+}> = (props) => {
+  return (
+    <button
+      class={`
+        bg-white
+        text-black
+        font-medium
+        text-[1.2rem]
+        px-12
+        py-[0.875rem]
+        rounded-lg
+        z-50
+        ${props.class ? props.class : ""}
+      `}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+const SexyButton: Component<{
+  children: JSX.Element;
+  class?: string;
+}> = (props) => {
+  return (
+    <div class="relative">
+      <div class="absolute background-goo blur-3xl w-52 h-12" />
+      <button
+        class={`
+          text-white
+          font-medium
+          text-[1.2rem]
+          rounded-lg
+          background-goo
+          p-[4px]
+          absolute
+          w-52
+          ${props.class ? props.class : ""}
+        `}
+      >
+        <div
+          class="
+          button-sexy-div-p
+          rounded-md
+          py-[0.875rem]
+          bg-black
+          hover:bg-transparent
+          hover:text-black
+          transition-colors
+          duration-200
+        "
+        >
+          {props.children}
+        </div>
+      </button>
+    </div>
+  );
+};
+
+export { PrimaryButton, SexyButton };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,20 @@
 import type { Component } from "solid-js";
 
+import { PrimaryButton, SexyButton } from "../components/Button";
+
 const HomePage: Component = () => {
   return (
     <div class="h-full flex flex-col items-center justify-center">
-      <h1 class="heading text-8xl font-extrabold tracking-tight text-goo">
+      <h1 class="heading text-6xl md:text-8xl font-extrabold tracking-tight text-goo">
         Kabootar
       </h1>
+
+      <div class="h-14" aria-hidden />
+
+      <div class="flex flex-col space-y-5 w-52">
+        <PrimaryButton class="z-10">Share a file</PrimaryButton>
+        <SexyButton class="w-52">Discover</SexyButton>
+      </div>
     </div>
   );
 };
