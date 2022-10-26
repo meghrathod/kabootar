859e8f1e3f6ea5d96aeb48b2a594bad19150146d
Akshit Garg <garg.akshit@gmail.com>
2022-10-27 00:40:04 +0530

    use staging endpoint
    Signed-off-by: Akshit Garg <garg.akshit@gmail.com>

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ export const iceServers: RTCConfiguration = {
   iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
 };
 
-export const secure = false; // TODO(AG): Flip this to true once we have a tailscale setup
-export const baseURL = "lithium.ts:5000";
+export const secure = true; // TODO(AG): Flip this to true once we have a tailscale setup
+export const baseURL = "signaling.staging.kabootar.potatokitty.me";
 export const httpScheme = secure ? "https://" : "http://";
 export const wsScheme = secure ? "wss://" : "ws://";
